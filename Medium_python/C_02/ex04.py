640c35d2b8652af1abc595565fbe67955655a966
Diogo Nogueira Lima <diogo.ngr.lima@gmail.com>
2024-03-26 23:19:31 -0300

    translate every word to english

--- Medium_python/C_02/ex04.py
+++ Medium_python/C_02/ex04.py
@@ -4,7 +4,7 @@ traveler_ids = [('USA', '31195855'), ('BRA', 'CE342567'), ('ESP', 'XDA205856')]
 for passport in sorted(traveler_ids): 
     print('%s/%s' % passport)
 '''
-parecido com:
+looks like:
 for place, num in sorted(traveler_ids): 
     print(f'{place} {num}')
 '''
--- Medium_python/C_02/ex05.py
+++ Medium_python/C_02/ex05.py
@@ -1,9 +1,9 @@
-# Exemplo de descompactação com atribuição para duas variáveis
+# Example of unpacking with assignment to two variables
 dados = (1, 2, 3)
 
-# Atribuição paralela usando descompactação para duas variáveis
-a, b = dados # erro
+# Parallel assignment using unpacking for two variables
+a, b, _ = dados  # Add an extra variable to handle the remaining value in the tuple
 
-# Resultado
+# Result
 print(a)  # Output: 1
 print(b)  # Output: 2
