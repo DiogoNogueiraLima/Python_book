845ea32cfe314e74dc855d21d51c7f9c8e663d5f
Diogo Nogueira Lima <diogo.ngr.lima@gmail.com>
2024-03-26 23:25:44 -0300

    fixing a mistake

--- Medium_python/C_02/ex05.py
+++ Medium_python/C_02/ex05.py
@@ -1,8 +1,8 @@
 # Example of unpacking with assignment to two variables
-dados = (1, 2, 3)
+data = (1, 2, 3)
 
 # Parallel assignment using unpacking for two variables
-a, b, _ = dados  # Add an extra variable to handle the remaining value in the tuple
+a, b, _ = data  # Add an extra variable to handle the remaining value in the tuple
 
 # Result
 print(a)  # Output: 1
